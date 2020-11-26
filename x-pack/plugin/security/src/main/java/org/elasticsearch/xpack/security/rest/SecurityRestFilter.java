d8f36cf52f81a121e400327545a8719fb0df98c7
Tim Vernum <tim@adjective.org>
2020-11-26 16:48:26 +1100

    [POC] Filter warning headers on 401 responses

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/SecurityRestFilter.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/SecurityRestFilter.java
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.util.Supplier;
 import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.util.Maps;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.common.xcontent.MediaType;
 import org.elasticsearch.common.xcontent.MediaTypeRegistry;
@@ -22,16 +23,15 @@ import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestHandler;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestRequest.Method;
-import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.rest.RestRequestFilter;
-
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.xpack.security.authc.AuthenticationService;
 import org.elasticsearch.xpack.security.authc.support.SecondaryAuthenticator;
 import org.elasticsearch.xpack.security.transport.SSLEngineUtils;
 
 import java.io.IOException;
-
 import java.util.List;
+import java.util.Map;
 
 public class SecurityRestFilter implements RestHandler {
 
@@ -100,6 +100,13 @@ public class SecurityRestFilter implements RestHandler {
                 @Override
                 protected boolean skipStackTrace() { return restStatus == RestStatus.UNAUTHORIZED; }
 
+                @Override
+                public Map<String, List<String>> filterHeaders(Map<String, List<String>> headers) {
+                    if (headers.containsKey("Warning")) {
+                        return Maps.copyMapWithRemovedEntry(headers, "Warning");
+                    }
+                    return headers;
+                }
             });
         } catch (Exception inner) {
             inner.addSuppressed(e);
