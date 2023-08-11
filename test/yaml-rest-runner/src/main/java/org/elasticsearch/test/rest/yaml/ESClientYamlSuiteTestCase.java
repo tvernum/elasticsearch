2a153db57ad5485b36a569812837c9a842b57c6d
Tim Vernum <tim@adjective.org>
2023-08-11 16:28:38 +1000

    Use component template if required in Yaml tests
    If legacy templates are unavailable, use a component template for the
    "global" index settings

--- test/yaml-rest-runner/src/main/java/org/elasticsearch/test/rest/yaml/ESClientYamlSuiteTestCase.java
+++ test/yaml-rest-runner/src/main/java/org/elasticsearch/test/rest/yaml/ESClientYamlSuiteTestCase.java
@@ -483,33 +483,39 @@ public abstract class ESClientYamlSuiteTestCase extends ESRestTestCase {
             inFipsJvm() && testCandidate.getTestSection().getSkipSection().getFeatures().contains("fips_140")
         );
 
-        if (ESRestTestCase.has(ProductFeature.LEGACY_TEMPLATES)) {
-            final Settings globalTemplateSettings = getGlobalTemplateSettings(
-                testCandidate.getTestSection().getSkipSection().getFeatures()
-            );
-            if (globalTemplateSettings.isEmpty() == false) {
-                final XContentBuilder template = jsonBuilder();
-                template.startObject();
-                {
-                    template.startArray("index_patterns").value("*").endArray();
-                    template.startObject("settings");
-                    globalTemplateSettings.toXContent(template, ToXContent.EMPTY_PARAMS);
+        final Settings globalTemplateSettings = getGlobalTemplateSettings(testCandidate.getTestSection().getSkipSection().getFeatures());
+        if (globalTemplateSettings.isEmpty() == false) {
+            boolean useComponentTemplate = ESRestTestCase.has(ProductFeature.LEGACY_TEMPLATES) == false;
+
+            final XContentBuilder template = jsonBuilder();
+            template.startObject();
+            {
+                template.startArray("index_patterns").value("*").endArray();
+                if (useComponentTemplate) {
+                    template.startObject("template");
+                }
+                template.startObject("settings");
+                globalTemplateSettings.toXContent(template, ToXContent.EMPTY_PARAMS);
+                template.endObject();
+                if (useComponentTemplate) {
                     template.endObject();
                 }
-                template.endObject();
-
-                final Request request = new Request("PUT", "/_template/global");
-                request.setJsonEntity(Strings.toString(template));
-                // Because this has not yet transitioned to a composable template, it's possible that
-                // this can overlap an installed composable template since this is a global (*)
-                // template. In order to avoid this failing the test, we override the warnings handler
-                // to be permissive in this case. This can be removed once all tests use composable
-                // templates instead of legacy templates
-                RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
-                builder.setWarningsHandler(WarningsHandler.PERMISSIVE);
-                request.setOptions(builder.build());
-                adminClient().performRequest(request);
             }
+            template.endObject();
+
+            final Request request = new Request("PUT", useComponentTemplate ? "/_index_template/global" : "/_template/global");
+            request.setJsonEntity(Strings.toString(template));
+            // Because not all case have transitioned to a composable template, it's possible that
+            // this can overlap an installed composable template since this is a global (*)
+            // template. In order to avoid this failing the test, we override the warnings handler
+            // to be permissive in this case. This can be removed once all tests use composable
+            // templates instead of legacy templates
+            RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+            if (useComponentTemplate == false) {
+                builder.setWarningsHandler(WarningsHandler.PERMISSIVE);
+            }
+            request.setOptions(builder.build());
+            adminClient().performRequest(request);
         }
 
         if (skipSetupSections() == false && testCandidate.getSetupSection().isEmpty() == false) {
